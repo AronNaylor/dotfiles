9e6cf0ee620cb049294922d8513e76399518772f
AronNaylor <aron.naylor@starlingbank.com>
2025-03-31 09:34:15 +0100

    copilot setup

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,21 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 -- inc-rename
 map("n", "<leader>ir", function()
   return ":IncRename " .. vim.fn.expand("<cword>")
 end, { expr = true })
+
+-- Enable Copilot
+map("n", "<leader>ae", ":Copilot enable<CR>", { desc = "Enable Copilot", unpack(opts) })
+
+-- Disable Copilot
+map("n", "<leader>ad", ":Copilot disable<CR>", { desc = "Disable Copilot", unpack(opts) })
+
+-- Show Copilot status
+map("n", "<leader>as", ":Copilot status<CR>", { desc = "Show Copilot status", unpack(opts) })
+
+-- Open Copilot Chat model selection
+map("n", "<leader>ac", ":Copilot chat model<CR>", { desc = "Open Copilot Chat model selection", unpack(opts) })
